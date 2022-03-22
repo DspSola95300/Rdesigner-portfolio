4015fa978178a2d6c7537a464d25cb827b9cd96b
DspSola <robin.sola@gmail.com>
2022-03-22 12:11:25 +0100

    Nouvelle barre de nav

--- Rdesigner(Portfolio)/app.js
+++ Rdesigner(Portfolio)/app.js
@@ -42,7 +42,7 @@ function isElemmentInViewport(el) {
     );
 }
 
-//menu navigation manager
+//Scroll
 
 // voir le scroll en px
 window.addEventListener('scroll', function (e){
@@ -53,13 +53,13 @@ window.addEventListener('scroll', function (e){
 
 //function de changement au scroll
 window.onscroll = function () {
-    if (document.documentElement.scrollTop >= 659 && document.documentElement.scrollTop < 1416) {
+    if (document.documentElement.scrollTop >= 721 && document.documentElement.scrollTop < 1443) {
         document.getElementsByClassName('navbar-bg')[0].style.top = "0px";
         document.getElementsByClassName('navbar')[0].style.top = "0px";
         document.getElementsByClassName('logo')[0].style.top = "0px";
         document.getElementsByClassName('background-mokup')[0].style.backgroundPositionX = "-150px";
     }
-    else if (document.documentElement.scrollTop >= 31 && document.documentElement.scrollTop < 659) {
+    else if (document.documentElement.scrollTop >= 31 && document.documentElement.scrollTop < 721) {
         document.getElementsByClassName('navbar-bg')[0].style.top = "-120px";
         document.getElementsByClassName('navbar')[0].style.top = "-120px";
         document.getElementsByClassName('logo')[0].style.top = "-120px";
@@ -75,3 +75,30 @@ window.onscroll = function () {
     scroll(loop); 
     // what is scroll() ?
 }
+
+//nav indicator animation
+const indicator = document.querySelector('.nav-indicator');
+const items = document.querySelectorAll('.nav-item');
+
+function handleIndicator(el){
+    //Boucler sur tous les items -> retirer la classe "is-active"
+    items.forEach(item => {
+        item.classList.remove('is-active');
+        item.removeAttribute('style');
+    })
+    //Styliser l'indicateur
+    indicator.style.width = `${el.offsetWidth}px`;
+    indicator.style.backgroundColor = '#D19D4A';
+    indicator.style.left = `${el.offsetLeft}px`;
+    //Ajoute la classe is-active
+    el.classList.add('is-active');
+    el.style.color = '#5B83A6';
+}
+
+items.forEach((item, index) => {
+    item.addEventListener('click', (e) => {
+        handleIndicator(e.target)
+        // console.log("item : "+ e.target)
+    });
+    item.classList.contains('is-active') && handleIndicator(item);
+});
